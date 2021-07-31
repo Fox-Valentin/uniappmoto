f18f250431bc1c4c6927904948d56bd546da798c
SmithXiong <xm864802180@163.com>
2021-07-31 18:00:43 +0800

    资讯详情，社群下拉框

--- components/index/info-list.vue
+++ components/index/info-list.vue
@@ -3,7 +3,7 @@
 		<block v-for="item in listData" :key="item.id">
 			<view class="uni-list-cell" hover-class="uni-list-cell-hover" @click="goDetail(item)">
 				<view class="uni-info-list" v-if="item.displayMode === 1">
-					<view class="uni-info-list-title uni-ellipsis-1"><text>{{ item.title }}</text></view>
+					<view class="uni-info-list-title uni-ellipsis-1"><text decode="true">{{ item.title }}</text></view>
 					<view v-if="item.titleImg.length === 1" class="uni-info-list-bottom">
 						<image class="uni-info-list-image-bottom" :src="item.titleImg[0]"></image>
 					</view>
@@ -15,7 +15,7 @@
 				</view>
 				<view class="uni-info-list row" v-else>
 					<view class="uni-info-list-body">
-						<view class="uni-info-list-title uni-ellipsis-1"><text>{{ item.title }}</text></view>
+						<view class="uni-info-list-title uni-ellipsis-1"><text decode="true">{{ item.title }}</text></view>
 						<view class="uni-info-list-text-bottom uni-ellipsis-3">
 							<text>{{ item.synopsis }}</text>
 						</view>
@@ -29,15 +29,17 @@
 
 <script>
 	export default {
-		props:{
-			listData:{
-				type:Array,
-				default:[]
+		props: {
+			listData: {
+				type: Array,
+				default: []
 			}
 		},
-		methods:{
-			goDetail(data){
-				
+		methods: {
+			goDetail(data) {
+				uni.navigateTo({
+					url: '../../pages/index/index-info?id=' + data.id
+				});
 			},
 		}
 	}
@@ -45,29 +47,29 @@
 
 <style scoped lang="scss">
 	.uni-info-list {
-	    padding: 11px 15px;
-	    box-sizing: border-box;
-	    display: flex;
-	    width: 100%;
-	    flex-direction: column;
+		padding: 11px 15px;
+		box-sizing: border-box;
+		display: flex;
+		width: 100%;
+		flex-direction: column;
 	}
-	
+
 	.row {
 		flex-direction: row;
 	}
-	
+
 	.uni-info-list-image-bottom {
 		width: 100%;
 		height: 320rpx;
 		border-radius: 10rpx;
 	}
-	
+
 	.uni-info-list-image-bottom {
 		width: 100%;
 		height: 320rpx;
 		border-radius: 10rpx;
 	}
-	
+
 	.uni-info-list-image-bottom3 {
 		width: 33%;
 		height: 220rpx;
@@ -75,7 +77,7 @@
 		padding: 0 10rpx;
 		box-sizing: border-box;
 	}
-	
+
 	.uni-info-list-image-right {
 		width: 180rpx;
 		height: 180rpx;
@@ -88,21 +90,21 @@
 		flex: 1;
 		padding-right: 10rpx;
 	}
-	
+
 	.uni-ellipsis-1 {
-	    overflow: hidden;
-	    text-overflow: ellipsis;
-	    display: -webkit-box;
-	    -webkit-line-clamp: 1;
-	    -webkit-box-orient: vertical;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		display: -webkit-box;
+		-webkit-line-clamp: 1;
+		-webkit-box-orient: vertical;
 	}
-	
+
 	.uni-ellipsis-3 {
-	    overflow: hidden;
-	    text-overflow: ellipsis;
-	    display: -webkit-box;
-	    -webkit-line-clamp: 3;
-	    -webkit-box-orient: vertical;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		display: -webkit-box;
+		-webkit-line-clamp: 3;
+		-webkit-box-orient: vertical;
 	}
 
 	.uni-info-list-title {
